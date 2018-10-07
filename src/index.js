a11b5232775039847cb09d8d71d2508eca9d92b9
Sanyu <sa.los@mail.ru>
2018-10-07 23:39:18 +0300

    Peppa

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+   if (array.length == 0) return 0;
+     let obj = {};
+    for (let i = 0; i < array.length; i++) {
+        obj[array[i]] = "number";
+    }
+     let count = 1;
+    let value = 1;
+    for (let key in obj) {
+        if (obj[key - 1]) {
+            value++;
+            continue;
+        }
+        if (value == 1) continue;
+        count = Math.max(count, value);
+        value = 1;
+    }
+    return count;
 }
+
